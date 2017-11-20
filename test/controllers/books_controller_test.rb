19bc8151e83501792e181a00acf392e43d30fd6d
Dan Roberts <dan@adadevelopersacademy.org>
2017-11-19 22:59:08 -0800

    Generate books controller and add tests

---
+++ test/controllers/books_controller_test.rb
@@ -0,0 +1,74 @@
+require "test_helper"
+
+describe BooksController do
+  describe "index" do
+    it "gives all the books" do
+      get books_url
+      must_respond_with :success
+
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.length.must_equal Book.count
+
+      keys = %w(id, title, author, publication_year).sort
+      body.each do |book|
+        book.keys.sort.must_equal keys
+      end
+    end
+
+    it "returns an empty array if no books exist" do
+      Book.destroy_all
+
+      get books_url
+      must_respond_with :success
+
+      response.header['Content-Type'].must_include 'json'
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.length.must_equal 0
+    end
+  end
+
+  describe "create" do
+    let (:raw_book) {{title: 'test book', author: 'tester', publication_year: 1999}}
+
+    it "creates a book from valid data" do
+      # Assumption: book data is valid
+      Book.new(raw_book).must_be :vaild?
+
+      start_count = Book.count
+
+      post books_url, params: { book: raw_book }
+      must_respond_with :success
+
+      Book.count.must_equal start_count + 1
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.keys.must_include "id"
+
+      book = Book.last
+      book.id.must_equal body["id"]
+      book.title.must_equal raw_book[:title]
+    end
+
+    it "returns an error and creates no book from invalid data" do
+      # Assumption: book data is invalid with no title
+      raw_book[:title] = ""
+      Book.new(raw_book).wont_be :vaild?
+
+      start_count = Book.count
+
+      post books_url, params: { book: raw_book }
+      must_respond_with :error
+
+      Book.count.must_equal start_count + 1
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Hash
+      body.keys.must_include "errors"
+      body["errors"].must_include "title"
+    end
+  end
+end
